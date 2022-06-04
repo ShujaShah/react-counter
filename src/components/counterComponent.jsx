f902921ed1708ca5ce80017b71b61533b4b6589d
ShujaShah <shuja.ashraf11@gmail.com>
2022-06-04 13:03:17 +0530

    Increment operation

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -3,24 +3,32 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
   };
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
 
   styles = {
     fontWeight: "bold",
     fontSize: 10,
   };
 
+  handleIncrement = () => {
+    console.log("button clicked", this);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
   }
